3a005cf33942e09cdd6c137c69b62e4a64613435
Gabriel <gabriel54178906@aluno.ms.senac.br>
2024-10-17 21:40:38 -0400

    nova versão

--- src/pages/login-professor.tsx
+++ src/pages/login-professor.tsx
@@ -1,9 +1,29 @@
 export const LoginProfessor = () => {
     return (
         <>
-            <div className="flex justify-center">
-                <h1 className="text-3xl font-bold underline">Página para o login dos professores</h1>
-            </div>
+            <main className="flex justify-center items-center">
+                <div className="layout-fundo">
+                    <div className="primeira-coluna">
+                        <h2>Bem-vindo ao sistema</h2>
+                        <p>Como professor, você deve informar o seu CPF e senha para efetuar o login</p>
+                        <p>Caso não tenho um login, clique em "Cadastrar-se"</p>
+                        <button>Cadastrar-se</button>
+                    </div>
+                    <div className="segunda-coluna">
+                        <form>
+                            <label htmlFor="">
+                                CPF:    
+                                <input type="number" name="cpf" id="cpf" />
+                                <br />
+                                Senha:  
+                                <input type="password" name="senha" id="senha" />
+                            </label>
+                            <br />
+                            <input type="submit"/>
+                        </form>
+                    </div>
+                </div>
+            </main>
         </>
     );
 };
